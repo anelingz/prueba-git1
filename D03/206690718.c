be3954aca953cfb3ffcd40ee3df1c8997929fb28
Felix Arreola Rodriguez <fgatuno.123@gmail.com>
2013-02-12 07:40:20 +0000

    Realizo practica 1
    * Creo programa 'hola mundo' en la seccion D03
    * Hago que git ignore los archivos *~
    * Agrego mi nombre a la lista

---
+++ D03/206690718.c
@@ -0,0 +1,27 @@
+/*
+ * 206690718.c
+ * This file is part of Practicas Git para Nancy
+ *
+ * Copyright (C) 2013 - Félix Arreola Rodríguez
+ *
+ * Practicas Git para Nancy is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Practicas Git para Nancy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Practicas Git para Nancy. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+intmain (int argc, char *argv[]) {
+	fprintf (stdout, "Hola mundo\n");
+	return EXIT_SUCCESS;
+}
